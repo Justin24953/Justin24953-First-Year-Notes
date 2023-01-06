84503919c2b08047bda81c8537427c592a9520c1
Justin24953 <121590009+Justin24953@users.noreply.github.com>
2023-01-05 19:31:41 -0500

    Strings

---
+++ WeeklyCoding/Javascript/Modern-Bootcamp/Basics/strings.js
@@ -0,0 +1,19 @@
+// A Variable is a way to store information in the the Global Memory to be used later.
+
+// let name = 'Levius Est'
+
+// console.log('name')
+
+//let firstName = 'Levius'
+//let lastName = 'Est'
+//let fullName = firstName +' ' + lastName
+
+// console.log(fullName)
+
+// Challenge Below
+let city =  'New York'
+let country = 'United States'
+let location = city + ', ' + country
+
+console.log (location)
+// Solution = New York, United States
