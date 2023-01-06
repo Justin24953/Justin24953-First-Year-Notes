11512e249466d9d9e6466bad8da0b7ca346b2014
Justin24953 <121590009+Justin24953@users.noreply.github.com>
2023-01-05 20:20:22 -0500

    Numbers

---
+++ WeeklyCoding/Javascript/Modern-Bootcamp/Basics/numbers.js
@@ -0,0 +1,16 @@
+// You don't need '' for numbers
+// Put numbers in () to chnage order of operations
+
+//let age = 21
+//let dogYears = (age + 1) / 7
+
+//console.log(dogYears)
+// Solution 3.142857142857143
+
+// Challenge
+let studentScore = 34
+let maxScore = 1000
+let percent = (studentScore / maxScore) * 100
+//percent = 3.4
+
+console.log(percent)
