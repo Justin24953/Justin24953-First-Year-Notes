0bbdc5a9bd68913d5952aee4ea7ff6599c1df43f
Justin24953 <121590009+Justin24953@users.noreply.github.com>
2023-01-05 20:36:59 -0500

    Variables

---
+++ WeeklyCoding/Javascript/Modern-Bootcamp/Basics/variables.js
@@ -0,0 +1,17 @@
+// A variable can't be defined more than once.
+let petName = 'Steve'
+
+petName = 'Steel'
+
+console.log(petName)
+
+// 1. Variable Names have rules
+
+// 2. Variable Names can't start with Numbers. Instead assign a number to the a variable.
+
+let 3 = 5
+let result = 3 + 2
+// ^The Computer doesn't know if this is a number or a variable yet.
+
+//3. Variable names cannont be reserved keywords.
+// Ex let let = 12
