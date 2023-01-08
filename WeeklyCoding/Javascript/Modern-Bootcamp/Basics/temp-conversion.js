5c58c6775b3526b4320de77301465862701d2581
Justin24953 <121590009+Justin24953@users.noreply.github.com>
2023-01-08 18:41:58 -0500

    Temp Project #1

--- WeeklyCoding/Javascript/Modern-Bootcamp/Basics/temp-conversion.js
+++ WeeklyCoding/Javascript/Modern-Bootcamp/Basics/temp-conversion.js
@@ -1,5 +1,10 @@
-// C = 42.3555556, K = 315.505556;
-let fahrenheit = 108.24 
-let kelvin = fahrenheit - 32 * 0.56 + 273.15
+// C = 55.5556, K = 328.706;
+let fahrenheit = 132 
+
+let celsius = (fahrenheit - 32) * 5 / 9
+
+let kelvin = (fahrenheit - 32) * 5 / 9 + 273.15
 
 console.log(celsius)
+
+console.log(kelvin)
