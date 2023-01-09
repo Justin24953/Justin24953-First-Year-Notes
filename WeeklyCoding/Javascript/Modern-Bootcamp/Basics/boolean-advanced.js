fe209483c39a5492af3a4faabb71a94331144efe
Justin24953 <121590009+Justin24953@users.noreply.github.com>
2023-01-08 22:31:08 -0500

    Advanced If statements

---
+++ WeeklyCoding/Javascript/Modern-Bootcamp/Basics/boolean-advanced.js
@@ -0,0 +1,26 @@
+let isAccountLocked = false
+let userRole = 'admin'
+
+if (isAccountLocked) {
+console.log('Your Account is locked')
+} else if (userRole === 'admin') {
+console.log('Welcome Admin')
+} else{
+console.log('Welcome Back')
+};
+
+// Challenge Area
+// Create a freezing statement
+// Create a Hot statement
+// Create a Ok Statment
+
+let temp = 64
+
+if (temp <= 32) {
+console.log("Stay inside it's freezing")
+} else if (temp >= 110) {
+  console.log("Stay inside it's too hot")
+} else (temp > 32) 
+{
+console.log("It's ok to go out.")
+}
