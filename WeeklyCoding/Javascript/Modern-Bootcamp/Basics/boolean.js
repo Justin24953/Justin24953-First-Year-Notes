666c4fae2f9703e7ecd578c0373959a812a1847b
Justin24953 <121590009+Justin24953@users.noreply.github.com>
2023-01-08 20:05:49 -0500

    Booleans and If statements

---
+++ WeeklyCoding/Javascript/Modern-Bootcamp/Basics/boolean.js
@@ -0,0 +1,44 @@
+//The equality operator (===) is used to compare two values or expressions. 
+// (!==) - Not Equal Operator.
+// < - less than operator
+// > - greator than operator
+// <= less tahn or equal to operator
+// >= greator than or equal to operator
+
+let temp = 133
+
+if (temp <= 32 ) {
+  console.log('Freezing Point')
+}
+
+if (temp >= 110) {
+console.log('You will melt')
+}
+
+// Challenge area
+
+// Create age set to your age.
+// Calculate is child - if they 7 or under.
+// Calculate is senior - if they are 65 or older.
+// Print is senior value.
+
+//let age = 70
+//let child = age < 18 
+//let adult =  age >= 18
+//let senior = age >= 65
+
+//console.log(child)
+//console.log(adult)
+//console.log(senior)
+
+// If 7 or under print message about child pricing 
+
+// If 65 or older print message about senior discount
+let age = 96
+if (age <= 7) {
+console.log('The Child gets half off')
+}
+
+if (age >= 62) {
+  console.log('The Senior gets 30% off')
+}
