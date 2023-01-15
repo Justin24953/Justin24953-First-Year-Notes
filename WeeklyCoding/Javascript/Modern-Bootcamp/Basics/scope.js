69b436c6a6dc62ace1da0944f97bcc1f8963e408
Justin24953 <121590009+Justin24953@users.noreply.github.com>
2023-01-14 19:12:14 -0500

    Understanding Scope

--- WeeklyCoding/Javascript/Modern-Bootcamp/Basics/scope.js
+++ WeeklyCoding/Javascript/Modern-Bootcamp/Basics/scope.js
@@ -2,11 +2,24 @@
 // Global Scope -  Everything outside code blocks.
 // Local Scope - Things defined inisde a code block.
 // In a scope you can access variables defined in that scope or, paret or ancestor scope.
+
+// Global SCope (varOne)
+// Local Scope (varTwo)
+// Local Scope (varFour)
+// Local Scope (varThree)
 let varOne = 'varOne'
 
 if (true) {
  console.log(varOne)
  let varTwo = 'varTwo'
+ console.log(varTwo)
+
+ if (true) {
+  let varFour = 'varFour'
+ }
 }
 
+if (true) {
+  let varThree = 'varThree'
+}
 console.log('varTwo')
