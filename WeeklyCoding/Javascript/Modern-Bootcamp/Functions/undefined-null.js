a03f88aca914cb8179823f7a9755688fb1f542e4
Justin24953 <121590009+Justin24953@users.noreply.github.com>
2023-01-15 16:53:53 -0500

    Null-Undefined

---
+++ WeeklyCoding/Javascript/Modern-Bootcamp/Functions/undefined-null.js
@@ -0,0 +1,24 @@
+// Undefined for ariable
+let name 
+
+if (name === undefined) {
+console.log('Give Name')
+}else{
+  console.log(name)
+}
+
+// Undefined for function arguments
+// Undefined as function return default value
+let sqaure = function(num) {
+  console.log(num)
+}
+
+let result = square()
+console.log(result)
+
+// Null as assigned value
+let age = 27
+
+age = null 
+
+console.log(age)
