dd9c44c8724943d2e84fcd06e7c51e32ad529b52
Justin24953 <121590009+Justin24953@users.noreply.github.com>
2023-01-22 20:12:31 -0500

    Introduction to Objects

---
+++ WeeklyCoding/Javascript/Modern-Bootcamp/Objects/objects-101.js
@@ -0,0 +1,31 @@
+let myBook = {
+  title: '1984',
+  author: 'George',
+  pageCount: 326,
+}
+
+    console.log(`The Author of the book ${myBook.title} is ${myBook.author}`)
+
+        myBook.title = 'Game of Thrones'
+
+          console.log(`The Author of the book ${myBook.title} is ${myBook.author}`)
+
+// Challenge area
+
+// name, age, location
+
+// Andrew is 27 and lives in Philadelhia
+
+//Increase age by 1 and print messe again
+
+let myProfile = {
+  name: 'Flint',
+  age: 28,
+  location: 'Flint Michigan',
+}
+
+    console.log(`Flint is ${myProfile.age} and lives in ${myProfile.location}`)
+        
+        myProfile.age = myProfile.age + 1
+
+            console.log(`Flint just turned ${myProfile.age} and still lives in ${myProfile.location}.`)
