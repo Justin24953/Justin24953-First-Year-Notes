2b8051a8cff74b827a94b19cc71e9b28a5643dc8
Justin24953 <121590009+Justin24953@users.noreply.github.com>
2023-01-26 20:06:48 -0500

    Methods

---
+++ WeeklyCoding/Javascript/Modern-Bootcamp/Objects/object-methods.js
@@ -0,0 +1,35 @@
+//let restaurant = {
+ // name: `Mario`,
+ // guestCapacity: 75,
+ // guestCount: 0,
+ // checkAvailability: function (partySize) {
+  //  let seatsLeft = this.guestCapacity - this.guestCount
+ //   return partySize <= seats
+ // }
+//}
+
+let status = restaurant.checkAvailability(4)
+
+console.log(status)
+
+
+let restaurant = {
+  name: 'ASB',
+  guestCapacity: 75,
+  guestCount: 0,
+  checkAvailability: function (partySize) {
+      let seatsLeft = this.guestCapacity - this.guestCount
+      return partySize <= seatsLeft
+  },
+  seatParty: function (partySize) {
+      this.guestCount = this.guestCount + partySize
+  },
+  removeParty: function (partySize) {
+      this.guestCount = this.guestCount - partySize
+  }
+}
+
+restaurant.seatParty(72)
+console.log(restaurant.checkAvailability(4))
+restaurant.removeParty(5)
+console.log(restaurant.checkAvailability(4))
