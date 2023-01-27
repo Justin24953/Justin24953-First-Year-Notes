bbb52b9e109ad05c100732f5b010dea40827bfa4
Justin24953 <121590009+Justin24953@users.noreply.github.com>
2023-01-26 19:34:59 -0500

    Expense Tracker

--- WeeklyCoding/Javascript/Modern-Bootcamp/Objects/object-references.js
+++ WeeklyCoding/Javascript/Modern-Bootcamp/Objects/object-references.js
@@ -10,8 +10,25 @@ let addExpense = function (account, amount) {
     console.log(account)
 }
 
-//addIncome
+let addIncome = function (account, income) {
+account.income = account.income +income
+}
 
+let resetAccount =  function (account) {
+  account.expenses = 0
+  account.income = 0
+}
+
+let getAccountSummary = function (account) {
+  let balance = account.income - account.expenses
+  return `Account for ${account.name} has ${balance}. ${account.income} in income. ${account.expenses} in expenses. }` 
+}
+
+addIncome(myAccount, 2000)
+addExpense(myAccount, 2.50)
+addExpense(myAccount, 160)
+
+console.log(getAccountSummary (myAccount))
 
 
 
