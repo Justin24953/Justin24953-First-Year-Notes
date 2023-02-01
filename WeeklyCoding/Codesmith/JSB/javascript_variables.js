8e3f3622c4676df4a489ef0d9134878a8878f741
Justin24953 <121590009+Justin24953@users.noreply.github.com>
2023-02-01 18:49:15 -0500

    Variable Review

---
+++ WeeklyCoding/Codesmith/JSB/javascript_variables.js
@@ -0,0 +1,13 @@
+// Variables 
+// Variables are 2 pieces Label and value. 
+// let name = `James`
+
+
+// You can seperate the declaration of variables.
+let name = `Steve`;
+let middleName = `Count`;
+
+let lastName;
+lastName = `Flare`;
+
+name = `Rick`
