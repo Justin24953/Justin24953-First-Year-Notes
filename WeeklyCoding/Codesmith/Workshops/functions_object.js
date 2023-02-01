3a02fc421173e380f5a6333fac8ef362337ca404
Justin24953 <121590009+Justin24953@users.noreply.github.com>
2023-01-31 20:08:12 -0500

    Codesmith

---
+++ WeeklyCoding/Codesmith/Workshops/functions_object.js
@@ -0,0 +1,20 @@
+// JavaScript The Easy Parts
+
+// Objects allow us to encapsulate information (data) and functionality
+
+// Functions allow us to bundle frequently used code, allowing us to make our code  more generalized.
+
+function averageGrade () {
+
+const myGrades = [50, 75, 100];
+
+let sum = 0;
+
+sum = myGrade[0] + myGrades[1] + myGrades[2]
+
+let average = sum / myGrades.length;
+ return average
+}
+
+const output = averageGrade()
+const newOutput = averageGrade()
