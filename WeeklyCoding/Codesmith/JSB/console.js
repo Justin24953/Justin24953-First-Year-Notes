d15eb195b9da565b4e99137b80b7044bc31f268b
Justin24953 <121590009+Justin24953@users.noreply.github.com>
2023-02-01 20:05:45 -0500

    Javascript Review

---
+++ WeeklyCoding/Codesmith/JSB/console.js
@@ -0,0 +1,14 @@
+// This is a single line comment
+
+//Global Memory
+// favFood:`Tofu` 
+// favFood: Tofu => French Fries
+
+let favFood = `Tofu`;
+console.log(favFood);
+favFood = `French Fries`
+console.log(favFood)
+
+//Console.log()
+// Tofu
+//French Fries
--- WeeklyCoding/Codesmith/JSB/gotchas.js
+++ WeeklyCoding/Codesmith/JSB/gotchas.js
@@ -4,6 +4,8 @@
 
 // Semicolons - it's best to end each line with them.
 
-//Errors - These might be intimidating but we will practice udnerstanding them. Use them to find to level up.
+//(Errors) - These might be intimidating but we will practice udnerstanding them. Use them to find to level up.
 
-//Syntax - Rules/Structure of how code is written (grammer for how we write code)
+//(Syntax) - Rules/Structure of how code is written (grammer for how we write code)
+
+// (The Console) The console is a tool we as developers use to peek into JS and see what the values are of variables are at specific times while running code.
