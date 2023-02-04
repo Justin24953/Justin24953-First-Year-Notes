8b98d59fd1fd4444641e93ef94e072c5880ae58e
Justin24953 <121590009+Justin24953@users.noreply.github.com>
2023-02-03 20:59:48 -0500

    Javascript Review Strings

---
+++ WeeklyCoding/Codesmith/CSX/Precourse-Part-1/challege.strings.js
@@ -0,0 +1,19 @@
+// Uncomment the lines below to see examples of strings
+console.log('A string can be written inside of single quotes.');
+// console.log("It can also be written inside of double quotes.");
+ console.log(`Backticks work, too.`);
+
+// ADD CODE BELOW
+let mySingleString = 'Single'
+let myDoubleString = "Double"
+let myBackString = `Back`
+
+
+
+// Uncomment the line below to check your work!
+ console.log(mySingleString);
+ console.log(myDoubleString);
+ console.log(myBackString);
+
+
+ 
