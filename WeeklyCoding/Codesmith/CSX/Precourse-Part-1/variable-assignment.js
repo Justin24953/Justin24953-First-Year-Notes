b3b3add4786e7e6da3de4387c9b2324fcc88d0ab
Justin24953 <121590009+Justin24953@users.noreply.github.com>
2023-02-03 20:59:58 -0500

    JavaScript Review

---
+++ WeeklyCoding/Codesmith/CSX/Precourse-Part-1/variable-assignment.js
@@ -0,0 +1,32 @@
+var firstName = 'Jenny';
+
+// Try un-commenting the line below
+ console.log("Hi, " + firstName)
+
+// ADD CODE HERE
+const lastName = 'Quick';
+
+
+// Uncomment the line below to check your work!
+ console.log("Hi, " + firstName + " " + lastName);
+
+
+// Declare a variable favFood and assign it a value of "tacos"
+// ADD CODE HERE
+let favFood = 'tacos'
+
+// Try un-commenting the line below to test your answer
+ console.log("My favorite food is " + favFood);
+
+// ADD CODE HERE
+favFood = 'Pizza'
+
+// Uncomment the line below to check your work!
+console.log("No, but really, my favorite food is " + favFood);
+
+// Declare a constant myCodingConfidence and assign it a value of "unwavering"
+// ADD CODE HERE
+const myCodingConfidence = 'unwavering'
+
+// Try un-commenting the line below to test your answer
+ console.log("My confidence in my coding ability is " + myCodingConfidence);
