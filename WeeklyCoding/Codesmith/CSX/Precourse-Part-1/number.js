d942a480f300e17fc74a3f081931191079bb2b2c
Justin24953 <121590009+Justin24953@users.noreply.github.com>
2023-02-06 18:58:31 -0500

    Numbers Add/Sub

--- WeeklyCoding/Codesmith/CSX/Precourse-Part-1/number.js
+++ WeeklyCoding/Codesmith/CSX/Precourse-Part-1/number.js
@@ -1,3 +1,4 @@
+
 //Create a variable birthYear and set it equal to the year of your birth.
 
 //Next, create a variable named age and set the value to your current age.
@@ -8,11 +9,9 @@ const birthYear = 1930;
 	let age = 92
   	let in10Yrs = age + 10
     
-
-
-
-
-
  console.log(birthYear);
  console.log(age);
  console.log(in10Yrs);
+
+
+
