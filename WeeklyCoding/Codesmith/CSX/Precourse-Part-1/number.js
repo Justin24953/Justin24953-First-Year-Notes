7684dbc056b3c4b23aa8dde67eb6ee51407692b1
Justin24953 <121590009+Justin24953@users.noreply.github.com>
2023-02-06 19:23:13 -0500

    Exponents & Remainders

--- WeeklyCoding/Codesmith/CSX/Precourse-Part-1/number.js
+++ WeeklyCoding/Codesmith/CSX/Precourse-Part-1/number.js
@@ -17,7 +17,6 @@ const birthYear = 1930;
 
  // -----Challenge: Numbers - Multiplication/Division-----
 
- 
 const twentyTimesFour = 20 * 4;
   const nineOverThree = 9 / 3;
     let twoHundredTimesThree = 200.3 * 3;
@@ -25,4 +24,22 @@ const twentyTimesFour = 20 * 4;
 console.log(twentyTimesFour);
 console.log(nineOverThree);
 console.log(twoHundredTimesThree);
+
+// -----Challenge: Numbers - Exponents & Remainders-----
+
+// The exponentiation operator (**) raises the first number to the power of the second number.
+
+// The modulus (remainder) operator (%) returns the remainder of dividing the first number by the second number.
+
+const fiveSquared = 5**2
+	const eightCubed = 8**3
+  	const sevenToTheSixth = 7**6
+    	const eightyThreeModSix = 83%6
+      	const sixtyModNine = 60%9
+
+console.log(fiveSquared)
+	console.log(eightCubed)
+		console.log(sevenToTheSixth)
+			console.log(eightyThreeModSix)
+				console.log(sixtyModNine)
 
