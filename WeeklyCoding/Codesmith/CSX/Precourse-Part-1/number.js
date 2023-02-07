f03419b74f877d1d073c340b1deda9b1ca5d9446
Justin24953 <121590009+Justin24953@users.noreply.github.com>
2023-02-06 19:45:30 -0500

    Assignment Operators

--- WeeklyCoding/Codesmith/CSX/Precourse-Part-1/number.js
+++ WeeklyCoding/Codesmith/CSX/Precourse-Part-1/number.js
@@ -57,5 +57,30 @@ let num1 = 32;
 
 console.log(num1)
 	console.log(num2)
+
+// -----Challenge: Numbers - Assignment Operators-----
+
+// += - addition
+// -= - subtraction
+// *= - multiplication
+// /= - division
+
+let num1 = 32;
+	let num2 = 25;
+		let num3 = 92;
+			let num4 = 64;
+
+// ADD CODE BELOW HERE
+num1 += 17
+	num2 -= 14
+		num3 *= 11
+			num4 /= 8
+
+console.log(num1)
+	console.log(num2)
+		console.log(num3)
+			console.log(num4)
+
+      
 				
 
