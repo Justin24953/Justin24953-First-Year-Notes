a7a1e0d1714e0c40bb64166b16b556fa5b1fbad2
Justin24953 <121590009+Justin24953@users.noreply.github.com>
2023-02-20 20:54:28 -0500

    csbin

--- WeeklyCoding/Codesmith/JSB/functional.js/function-csbin.js
+++ WeeklyCoding/Codesmith/JSB/functional.js/function-csbin.js
@@ -1,1 +1,30 @@
 // -----CHALLENGE 1-----
+// -----CHALLENGE 1-----
+let greetingPt1 = 'Hello'
+let greetingPt2 = 'World'
+
+console.log(greetingPt1 + ' ' + greetingPt2)
+
+
+function greeting () {
+ let greetingPt1 = 'Hello'
+ let greetingPt2 = 'World'
+ return greetingPt1 + ' ' + greetingPt2
+};
+
+const welcome = greeting();
+
+console.log(welcome)
+
+// -----CHALLENGE 2-----
+
+function createPartnerGreeting (name) {
+ // let partnerName = 'Me, Myslef, and I'
+  return `Hey ${name}`
+};
+
+let partnerGreeting = createPartnerGreeting('Josh')
+
+console.log(partnerGreeting)
+
+// -----CHALLENGE 3-----
