38a09e68cca8418113996a6647ca78d7ad1ba097
Justin24953 <121590009+Justin24953@users.noreply.github.com>
2023-02-20 21:06:15 -0500

    csbin

--- WeeklyCoding/Codesmith/JSB/functional.js/function-csbin.js
+++ WeeklyCoding/Codesmith/JSB/functional.js/function-csbin.js
@@ -28,3 +28,18 @@ let partnerGreeting = createPartnerGreeting('Josh')
 console.log(partnerGreeting)
 
 // -----CHALLENGE 3-----
+function addTwo(num1,num2) {
+  let sumOfTwo = num1 + num2
+  return sumOfTwo
+}
+
+let sum = addTwo(5, 19)
+
+console.log(sum)
+
+
+
+
+//Uncomment the line below to check your work!
+console.log(addTwo(10, 20)); // --> 30
+console.log(addTwo(16, 199)); // --> 215
