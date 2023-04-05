da66d3bcf9e32f4b724f6b1903c5c8e4575ab65e
Justin24953 <121590009+Justin24953@users.noreply.github.com>
2023-04-04 20:30:56 -0400

    JS

---
+++ WeeklyCoding/Javascript/Modern-Bootcamp/Projects/Survivor-Cast/cast.js
@@ -0,0 +1,72 @@
+let castaway1 = {
+  name: "Matt Blankinship",
+  bootOrder: "NA",
+};
+let castway2 = {
+  name: "Claire Rafson",
+  bootOrder: "NA",
+};
+let castway3 = {
+  name: "Carolyn Wiger",
+  bootOrder: "NA",
+};
+let castway4 = {
+  name: "",
+  bootOrder: "NA",
+};
+let castway5 = {
+  name: "",
+  bootOrder: "NA",
+};
+let castway6 = {
+  name: "",
+  bootOrder: "NA",
+};
+let castway7 = {
+  name: "",
+  bootOrder: "NA",
+};
+let castway8 = {
+  name: "",
+  bootOrder: "NA",
+};
+let castway9 = {
+  name: "",
+  bootOrder: "NA",
+};
+let castway10 = {
+  name: "",
+  bootOrder: "NA",
+};
+let castway11 = {
+  name: "",
+  bootOrder: "NA",
+};
+let castway12 = {
+  name: "",
+  bootOrder: "NA",
+};
+let castway13 = {
+  name: "",
+  bootOrder: "NA",
+};
+let castway14 = {
+  name: "",
+  bootOrder: "NA",
+};
+let castway15 = {
+  name: "",
+  bootOrder: "NA",
+};
+let castway16 = {
+  name: "",
+  bootOrder: "NA",
+};
+let castway17 = {
+  name: "",
+  bootOrder: "NA",
+};
+let castway18 = {
+  name: "",
+  bootOrder: "NA",
+};
