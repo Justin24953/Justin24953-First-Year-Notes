8f7b6014a14f59975b6bd83f6441326753d37139
Justin24953 <121590009+Justin24953@users.noreply.github.com>
2023-04-04 22:46:35 -0400

    JS

--- WeeklyCoding/Javascript/Modern-Bootcamp/Projects/Survivor-Cast/cast.js
+++ WeeklyCoding/Javascript/Modern-Bootcamp/Projects/Survivor-Cast/cast.js
@@ -1,90 +1,71 @@
 let castaway1 = {
   name: "Matt Blankinship",
-  
 };
 let castaway2 = {
   name: "Claire Rafson",
-  
 };
 let castaway3 = {
   name: "Carolyn Wiger",
-  
 };
 let castaway4 = {
   name: "Yam Yam",
-  
 };
 let castaway5 = {
   name: "Lauren Harpe",
-  
 };
 let castaway6 = {
   name: "Danny Massa",
-  
 };
 let castaway7 = {
   name: "Heidi",
-  
 };
 let castaway8 = {
   name: "Carson Garrett",
-  
 };
 let castaway9 = {
   name: "Maddy Pomilla",
-  
 };
 let castaway10 = {
   name: "Bruce Perreault",
-  
 };
 let castaway11 = {
   name: "Sarah Wade",
-  
 };
 let castaway12 = {
   name: "Matthew Grinstead",
-  
 };
 let castaway13 = {
   name: "Helen",
-  
 };
 let castaway14 = {
   name: "Josh",
-  
 };
 let castaway15 = {
   name: "Jaime",
-  
 };
 let castaway16 = {
   name: "Kane",
-  
 };
 let castway17 = {
   name: "Frannie",
-  
 };
 let castaway18 = {
   name: "Brandon",
-  
 };
 
 function addKeyValue(obj, key, value) {
   if (obj.hasOwnProperty(key)) {
-  console.log("Key " + key + " already exists in the object.");
+    console.log("Key " + key + " already exists in the object.");
   } else {
     obj[key] = value;
   }
 }
 
-addKeyValue(castaway10,"bootOrder",1);
-addKeyValue(castaway9,"bootOrder",2);
-addKeyValue(castaway13,"bootOrder",3);
-addKeyValue(castaway2,"bootOrder",4);
-addKeyValue(castaway11,"bootOrder",5);
-addKeyValue(castaway12,"bootOrder",6);
+addKeyValue(castaway10, "bootOrder", 1);
+addKeyValue(castaway9, "bootOrder", 2);
+addKeyValue(castaway13, "bootOrder", 3);
+addKeyValue(castaway2, "bootOrder", 4);
+addKeyValue(castaway11, "bootOrder", 5);
+addKeyValue(castaway12, "bootOrder", 6);
 
-console.log(castaway10)
-
+console.log(castaway10);
