0cda07e97067fb3b35a4bb1dab896fcfab706acd
Justin24953 <121590009+Justin24953@users.noreply.github.com>
2023-04-22 20:37:57 -0400

    js

---
+++ WeeklyCoding/Javascript/Modern-Bootcamp/Projects/Survivor-Cast/cast.2.js
@@ -0,0 +1,48 @@
+const castaways = [
+  { name: "Matt Blankinship" },
+  { name: "Claire Rafson" },
+  { name: "Carolyn Wiger" },
+  { name: "Yam Yam" },
+  { name: "Lauren Harpe" },
+  { name: "Danny Massa" },
+  { name: "Heidi" },
+  { name: "Carson Garrett" },
+  { name: "Maddy Pomilla" },
+  { name: "Bruce Perreault" },
+  { name: "Sarah Wade" },
+  { name: "Matthew Grinstead" },
+  { name: "Helen" },
+  { name: "Josh" },
+  { name: "Jaime" },
+  { name: "Kane" },
+  { name: "Frannie" },
+  { name: "Brandon" },
+];
+
+const ratuTribe = [
+  { name: "Brandon",},
+  { name: "Kane" },
+  { name: "Matthew Grinstead" },
+  { name: "Jaime" },
+  { name: "Lauren Harpe" },
+  { name: "Maddy Pomilla" },
+];
+
+const sokaTribe = [
+  { name: "Claire Rafson" },
+  { name: "Frannie" },
+  { name: "Heidi" },
+  { name: "Danny Massa" },
+  { name: "Josh" },
+  { name: "Matthew Grinstead" },
+];
+
+const tikaTribe = [
+  { name: "Carolyn Wiger" },
+  { name: "Helen" },
+  { name: "Sarah Wade" },
+  { name: "Bruce Perreault" },
+  { name: "Carson Garrett" },
+  { name: "Yam Yam" },
+];
+
