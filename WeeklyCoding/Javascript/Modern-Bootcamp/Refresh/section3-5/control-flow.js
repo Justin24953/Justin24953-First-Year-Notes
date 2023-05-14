aa20231eea6c8fb2eb4c593e473a0ff1e0e691c5
Justin24953 <121590009+Justin24953@users.noreply.github.com>
2023-05-14 19:02:53 -0400

    Javascript Refresh

--- WeeklyCoding/Javascript/Modern-Bootcamp/Refresh/section3-5/control-flow.js
+++ WeeklyCoding/Javascript/Modern-Bootcamp/Refresh/section3-5/control-flow.js
@@ -15,7 +15,24 @@ console(`It's freezing outside`)
 let isAccountLocked = true
 
 if(isAccountLocked) {
-  console.log(`This is True`)
+  console.log(`Account is Locked`)
 } else if (!isAccountLocked) {
-console.log(`This is false`)
+console.log(`Account is Not Locked`)
+} else {
+  console.log(`Account doesn't hae a status.`)
+}
+
+// Logical And Operator
+// && Both Side have to be True. False otherwise
+//Example
+let tem = 65
+ 
+if (tem >= 60 && tem <= 90) {
+console.log(`it is pretty nice out`)
+}
+
+// Logical Or Operator 
+// || Only One side has to be true for the code to Pass
+if (tem <= 0 || temp >= 120) {
+  console.log(`Do not Go outside`)
 }
