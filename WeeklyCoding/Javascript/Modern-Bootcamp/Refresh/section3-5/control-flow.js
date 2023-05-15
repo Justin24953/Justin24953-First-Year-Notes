a41b2cf60c4a1e790ec605817e9eb6c360d58268
Justin24953 <121590009+Justin24953@users.noreply.github.com>
2023-05-14 21:40:45 -0400

    Javascript Refresh Scope

--- WeeklyCoding/Javascript/Modern-Bootcamp/Refresh/section3-5/control-flow.js
+++ WeeklyCoding/Javascript/Modern-Bootcamp/Refresh/section3-5/control-flow.js
@@ -25,14 +25,63 @@ console.log(`Account is Not Locked`)
 // Logical And Operator
 // && Both Side have to be True. False otherwise
 //Example
-let tem = 65
+temp = 65
  
-if (tem >= 60 && tem <= 90) {
+if (temp >= 60 && temp <= 90) {
 console.log(`it is pretty nice out`)
 }
 
 // Logical Or Operator 
 // || Only One side has to be true for the code to Pass
-if (tem <= 0 || temp >= 120) {
+if (temp <= 0 || temp >= 120) {
   console.log(`Do not Go outside`)
 }
+
+// Advanced If Statement With && and ||
+if (temp>=60 && temp <= 90) {
+  console(`It's a Pretty Good Day Today`)
+} else if (temp <= 0 || temp >= 100) {
+  console(`Stay Inside`)
+} else {
+  console(`Go Out Into The Unknown`)
+}
+
+// Variable Scope 
+//Global Scope
+  //Local Scope
+    //Local Scope
+  //Local Scope
+let varOne = `varOne`
+
+if (true) {
+  console.log(varOne)
+  let varTwo = `varTwo`
+  console.log(varTwo)
+  
+  if (true) {
+    let varThree = 'varThree'
+    console.log(varThree)
+  }
+}
+
+if (true) {
+  let varFour = 'varFour'
+  console.log(varFour)
+}
+
+// Variable Scope Part 2
+
+ //Global Scope
+  //Local Scope
+    //Local Scope
+  //Local Scope
+
+  if (true) {
+    if (true) {
+
+    }
+  }
+
+  if (true) {
+
+  }
