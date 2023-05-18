21e2aa716cfe17fa5cb31a5f10fb7eaec645af78
Justin24953 <121590009+Justin24953@users.noreply.github.com>
2023-05-17 22:29:22 -0400

    fix

--- WeeklyCoding/Javascript/Modern-Bootcamp/Refresh/section3-5/functions.js
+++ WeeklyCoding/Javascript/Modern-Bootcamp/Refresh/section3-5/functions.js
@@ -1,10 +1,21 @@
 // Basics
 
-// Function - input , code, output
+// Function - input(argument) , code, output(return)
 
-let  greetUser = function () {
-  console.log('Hi')
-    
-  }
-  
-  greetUser()
+let greetUser = function () {
+  console.log("Hi");
+};
+
+greetUser();
+
+// How to Call A fucntion/ How to Add An Argument/ How to called a return statment.
+
+let sqaure = function (num) {
+  let result = num * num
+  return result
+}
+
+let value = sqaure(3)
+
+console.log(value)
+
