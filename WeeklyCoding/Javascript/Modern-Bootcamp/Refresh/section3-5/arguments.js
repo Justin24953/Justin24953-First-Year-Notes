1151445bea765d09ea1a4f803b06b373942ca86c
Justin24953 <121590009+Justin24953@users.noreply.github.com>
2023-05-21 22:41:47 -0400

    Tip Calc

--- WeeklyCoding/Javascript/Modern-Bootcamp/Refresh/section3-5/arguments.js
+++ WeeklyCoding/Javascript/Modern-Bootcamp/Refresh/section3-5/arguments.js
@@ -15,3 +15,19 @@ let getScoreText = function (name = 'Player 1', score = 0) {
 }
 let scoreText = getScoreText('John Doe', 54) 
 console.log(scoreText)
+
+///Challenge: Tip Calculator 
+// total, tipPercent = .2 = 20%
+
+let tipCalc = function (total, tipPercent = .2) {
+  return total * tipPercent
+}
+
+let getTip = tipCalc(20,undefined)
+let getTip2 = tipCalc(30,.3)
+
+console.log(`Your Tip has been calculated $(${getTip})`)
+//"Your Tip has been calculated $(4)"
+console.log(`Your Tip has been calculated $(${getTip2})`)
+//"Your Tip has been calculated $(9)"
+
