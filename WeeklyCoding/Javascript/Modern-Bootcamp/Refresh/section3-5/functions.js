2700c3c5f8b69b9488bbbc583179950bf5e2ce58
Justin24953 <121590009+Justin24953@users.noreply.github.com>
2023-05-21 20:19:59 -0400

    Undefiend review

--- WeeklyCoding/Javascript/Modern-Bootcamp/Refresh/section3-5/functions.js
+++ WeeklyCoding/Javascript/Modern-Bootcamp/Refresh/section3-5/functions.js
@@ -12,50 +12,55 @@ greetUser();
 // How to Call A fucntion/ How to Add An Argument/ How to called a return statment.
 
 let sqaure = function (num) {
-  let result = num * num
-  return result
-}
+  let result = num * num;
+  return result;
+};
 
-let value = sqaure(3)
+let value = sqaure(3);
 
-console.log(value)
+console.log(value);
 
 // Convert Fahrenheit to Celsius
 
 // (30 -> 0) (68 -> 20)
 
+let ftc = function (fahrenheit) {
+  let celsius = ((fahrenheit - 32) * 5) / 9;
+  return celsius;
+};
 
-let ftc = function(fahrenheit) {
-  let celsius = (fahrenheit - 32) * 5 / 9
-  return celsius
-}
-
-let fTemp =  ftc(32)
+let fTemp = ftc(32);
 // 0
-let fTemp2 = ftc(68)
+let fTemp2 = ftc(68);
 // 20
-console.log(fTemp)
-console.log(fTemp2)
+console.log(fTemp);
+console.log(fTemp2);
 
 // Undefined and Null
 
 // Undefined for variable
-let name 
+let name;
 
 if (name === undefined) {
-  console.log('name is unknown')
+  console.log("name is unknown");
 } else {
-  console.log(name)
+  console.log(name);
 }
 
 // Undefined for function arguments
 // Undefined as fucntion default return value
 
 let sqaured = function (num) {
-  console.log(num)
-}
+  console.log(num);
+};
+// -> undefiend
 
-let result = sqaured()
-console.log(result)
+let result = sqaured();
+console.log(result);
+// -> undefined
 
+// Null as assigned value
+let age = 27;
+age = null;
 
+console.log(age);
