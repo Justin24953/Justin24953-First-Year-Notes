35703d4f3c08962ee320787a6f15d34de7485f78
Justin24953 <121590009+Justin24953@users.noreply.github.com>
2023-05-21 20:12:45 -0400

    Undefined review

--- WeeklyCoding/Javascript/Modern-Bootcamp/Refresh/section3-5/functions.js
+++ WeeklyCoding/Javascript/Modern-Bootcamp/Refresh/section3-5/functions.js
@@ -1,4 +1,4 @@
-// Basics
+// Function Basics
 
 // Function - input(argument) , code, output(return)
 
@@ -7,6 +7,7 @@ let greetUser = function () {
 };
 
 greetUser();
+// "Hi"
 
 // How to Call A fucntion/ How to Add An Argument/ How to called a return statment.
 
@@ -18,4 +19,43 @@ let sqaure = function (num) {
 let value = sqaure(3)
 
 console.log(value)
+
+// Convert Fahrenheit to Celsius
+
+// (30 -> 0) (68 -> 20)
+
+
+let ftc = function(fahrenheit) {
+  let celsius = (fahrenheit - 32) * 5 / 9
+  return celsius
+}
+
+let fTemp =  ftc(32)
+// 0
+let fTemp2 = ftc(68)
+// 20
+console.log(fTemp)
+console.log(fTemp2)
+
+// Undefined and Null
+
+// Undefined for variable
+let name 
+
+if (name === undefined) {
+  console.log('name is unknown')
+} else {
+  console.log(name)
+}
+
+// Undefined for function arguments
+// Undefined as fucntion default return value
+
+let sqaured = function (num) {
+  console.log(num)
+}
+
+let result = sqaured()
+console.log(result)
+
 
