d6397a994659da4c11768c790ff13e27d42c7cd3
Justin24953 <121590009+Justin24953@users.noreply.github.com>
2023-05-24 10:40:09 -0400

    Objects

---
+++ WeeklyCoding/Javascript/Modern-Bootcamp/Refresh/section3-5/objects.js
@@ -0,0 +1,17 @@
+// Object Basics
+
+let myBook = {
+  key: 'value',
+  title: 'Berzerk',
+  pages: 220
+}
+
+console.log(myBook)
+/*let myBook = {
+  key: 'value',
+  title: 'Berzerk',
+  pages: 220
+
+} */
+console.log(myBook.key)
+// 'value'
