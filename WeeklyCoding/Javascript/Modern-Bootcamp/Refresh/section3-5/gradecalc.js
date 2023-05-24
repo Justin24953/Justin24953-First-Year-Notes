de4a1431914e97dde4352aac2976fff3091eed8a
Justin24953 <121590009+Justin24953@users.noreply.github.com>
2023-05-23 20:19:44 -0400

    Calc

--- WeeklyCoding/Javascript/Modern-Bootcamp/Refresh/section3-5/gradecalc.js
+++ WeeklyCoding/Javascript/Modern-Bootcamp/Refresh/section3-5/gradecalc.js
@@ -59,3 +59,32 @@ console.log(studentD);
 let studentE = gradeCal(1, 20);
 console.log(studentE);
 // "You Got An F(5)"
+
+// Revised Version That Take Advantage of (Variables In If and Else Statements)
+
+let gradeCalcc = function (score, total) {
+  let percent = (score / total) * 100;
+  let letterGrade = null;
+
+  if (percent >= 90) {
+    letterGrade = "A";
+  } else if (percent >= 80) {
+    letterGrade = "B";
+  } else if (percent >= 70) {
+    letterGrade = "C";
+  } else if (percent >= 60) {
+    letterGrade = "D";
+  } else {
+    letterGrade = "F";
+  }
+  return `Your Grade is ${letterGrade} with a score of ${percent}`;
+};
+
+let studentF = gradeCalcc(90, 100);
+console.log(studentF);
+
+let studentG = gradeCalcc(80, 100);
+console.log(studentG);
+
+let studentH = gradeCalcc(70, 100);
+console.log(studentH);
