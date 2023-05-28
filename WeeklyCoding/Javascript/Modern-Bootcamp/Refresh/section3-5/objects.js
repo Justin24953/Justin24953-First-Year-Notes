580fd291c28348e94242484197229f3429762287
Justin24953 <121590009+Justin24953@users.noreply.github.com>
2023-05-27 21:43:58 -0400

    Objects review

--- WeeklyCoding/Javascript/Modern-Bootcamp/Refresh/section3-5/objects.js
+++ WeeklyCoding/Javascript/Modern-Bootcamp/Refresh/section3-5/objects.js
@@ -1,17 +1,20 @@
 // Object Basics
 
-let myBook = {
-  key: 'value',
-  title: 'Berzerk',
-  pages: 220
-}
 
-console.log(myBook)
-/*let myBook = {
-  key: 'value',
-  title: 'Berzerk',
-  pages: 220
-
-} */
 console.log(myBook.key)
 // 'value'
+
+let myBook = {
+  book:'Blade',
+  pageCount: 234,
+}
+
+// Temeplate Literals With Obejct Values
+console.log(`The Name of My book is ${myBook.book} and it has a page count of ${myBook.pageCount}`)
+// "The Name of My book is Blade and it has a page count of 234"
+
+// Reassign Object Values
+myBook.pageCount = 26
+console.log(myBook.pageCount)
+// 26
+
