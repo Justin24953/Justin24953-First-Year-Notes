2557c99a653ba4b5f96e9894dd9e4b1c3a6da124
Justin24953 <121590009+Justin24953@users.noreply.github.com>
2023-06-04 20:51:14 -0400

    Expense Project

--- WeeklyCoding/Javascript/Modern-Bootcamp/Refresh/section3-5/expense.js
+++ WeeklyCoding/Javascript/Modern-Bootcamp/Refresh/section3-5/expense.js
@@ -1,9 +1,9 @@
 //Expense Tracker
 let myAccount = {
-  name: 'John Snow',
+  name: "John Snow",
   expense: 0,
   income: 0,
-}
+};
 
 // addIncome
 
@@ -14,37 +14,34 @@ let myAccount = {
 
 // Order Of Testing
 // addIncome
-    let addIncome = function (account,total) {
-      account.income = account.income + total
-    }
-    let pay = addIncome(myAccount,60)
-
+let addIncome = function (account, total) {
+  account.income = account.income + total;
+};
+let pay = addIncome(myAccount, 60);
 
 // addExpense
-    let addExpense = function (account,total) {
-      account.expense = account.expense + total
-    }
+let addExpense = function (account, total) {
+  account.expense = account.expense + total;
+};
 
-    let tacoPrice = addExpense(myAccount,50)
-
+let tacoPrice = addExpense(myAccount, 50);
 
 // getAccountSummary
-    let accountSummary = function (account) {
-      return {
-        summary: `${myAccount.name} has an income of ${myAccount.income} and ${myAccount.expense} in expenses`
-      }
-    }
+let accountSummary = function (account) {
+  return {
+    summary: `${myAccount.name} has an income of ${myAccount.income} and ${myAccount.expense} in expenses`,
+  };
+};
+
 // resetAccount
-    let resetAccount = function(account) {
-      
-    }
+let resetAccount = function (account, reset) {
+  account.expense = reset;
+  account.income = reset;
+};
+let firstReset = resetAccount(myAccount, 0);
+
 // getAccountSummary
-    
-    
-let firstSum = accountSummary()
-console.log(firstSum)
 
-// Make reset
-
-
-
+let firstSum = accountSummary();
+console.log(firstSum);
+//   "summary": "John Snow has an income of 0 and 0 in expenses"
