01336397a0b90bdde4ba413c2bf59f24cc7435f6
Justin24953 <121590009+Justin24953@users.noreply.github.com>
2023-06-10 23:17:42 -0400

    Method Challenge

--- WeeklyCoding/Javascript/Modern-Bootcamp/Refresh/section3-5/methods.js
+++ WeeklyCoding/Javascript/Modern-Bootcamp/Refresh/section3-5/methods.js
@@ -3,13 +3,22 @@
 
 let restaurant = {
   name: `Jimmy's`,
-  guestCount: 75,
-  guestCount: 0,
+  guestCap: 75,
+  guestCount: 25,
   checkAvailability: function (partySize) {
-    return true
+    let seatsLeft = this.guestCap - this.guestCount
+    return partySize <= seatsLeft
   }
 }
 
-let status = restaurant.checkAvailability(true)
+let status = restaurant.checkAvailability(51)
 
 console.log(status)
+
+// Create New Methods
+
+// 1.  seatParty
+// 2. removeParty
+
+//Examples
+
