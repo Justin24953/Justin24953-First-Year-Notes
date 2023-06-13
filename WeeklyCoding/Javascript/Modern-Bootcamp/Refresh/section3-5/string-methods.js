1c4f5fd69bd76dc4a8d5e56922f79921fb86c001
Justin24953 <121590009+Justin24953@users.noreply.github.com>
2023-06-12 23:51:21 -0400

    string methods

---
+++ WeeklyCoding/Javascript/Modern-Bootcamp/Refresh/section3-5/string-methods.js
@@ -0,0 +1,10 @@
+let name = `John Snow`
+
+// Length Property
+console.log(name.length)
+// Remember that spaces count in length
+
+// Convert to upper case
+console.log (name.toUpperCase())
+
+// So far I realized that Properties work to recrieve infomation while methods are used to print or change that infomation. Examples include .log() and .toUpperCase(). Both of these unlike properties use ().
