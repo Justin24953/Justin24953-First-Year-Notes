2b9d8ccdbd1fe510749f26758c929940fea41298
Justin24953 <121590009+Justin24953@users.noreply.github.com>
2023-06-14 21:57:08 -0400

    String Method Challenge

--- WeeklyCoding/Javascript/Modern-Bootcamp/Refresh/section3-5/string-methods.js
+++ WeeklyCoding/Javascript/Modern-Bootcamp/Refresh/section3-5/string-methods.js
@@ -1,11 +1,11 @@
-let name = `John Snow`
+let name = `John Snow`;
 
 // Length Property
-console.log(name.length)
+console.log(name.length);
 // Remember that spaces count in length
 
 // Convert to upper case
-console.log (name.toUpperCase())
+console.log(name.toUpperCase());
 
 // So far I've realized that Properties work to recrieve infomation while methods function that allow us to do something to the data. An example of this would be a console.log() or name.toUpperCase()
 
@@ -16,14 +16,15 @@ console.log (name.toUpperCase())
 // length is more than 8 and it doesn't contain the word password.
 
 let isValidPassword = function (password) {
-if (password.length > 8 || password.includes(`John`)) {
-return `The Password ${password} doesn't meet our requirements`
-} else {
-  return `Your Password ${password} is valid`
-}
-}
+  if (password.length > 8 && password.includes(`John`)) {
+    return `The Password ${password} doesn't meet our requirements`;
+  } else {
+    return `Your Password ${password} is valid`;
+  }
+};
 
-console.log(isValidPassword('John'))
-console.log(isValidPassword('Jonny'))
+console.log(isValidPassword("John"));
+console.log(isValidPassword("Jonny"));
+
 
 // result The Password John doesn't meet our requirements
