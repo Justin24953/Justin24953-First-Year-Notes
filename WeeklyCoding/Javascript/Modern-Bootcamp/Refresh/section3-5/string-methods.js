1e03dce1a91898cb57d468b57b905d6be809e360
Justin24953 <121590009+Justin24953@users.noreply.github.com>
2023-06-14 21:51:30 -0400

    string methods challenge

--- WeeklyCoding/Javascript/Modern-Bootcamp/Refresh/section3-5/string-methods.js
+++ WeeklyCoding/Javascript/Modern-Bootcamp/Refresh/section3-5/string-methods.js
@@ -7,5 +7,23 @@ console.log(name.length)
 // Convert to upper case
 console.log (name.toUpperCase())
 
-// So far I realized that Properties work to recrieve infomation while methods are used to print or change that infomation. Examples include .log() and .toUpperCase(). Both of these unlike properties use ().
-// Git CHeck
+// So far I've realized that Properties work to recrieve infomation while methods function that allow us to do something to the data. An example of this would be a console.log() or name.toUpperCase()
+
+// Challenge area
+
+// isValidPassword
+
+// length is more than 8 and it doesn't contain the word password.
+
+let isValidPassword = function (password) {
+if (password.length > 8 || password.includes(`John`)) {
+return `The Password ${password} doesn't meet our requirements`
+} else {
+  return `Your Password ${password} is valid`
+}
+}
+
+console.log(isValidPassword('John'))
+console.log(isValidPassword('Jonny'))
+
+// result The Password John doesn't meet our requirements
