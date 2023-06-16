c160ebb70a30a57c123259aae6f7d83c9cc056c2
Justin24953 <121590009+Justin24953@users.noreply.github.com>
2023-06-15 22:14:30 -0400

    fixed num

---
+++ WeeklyCoding/Javascript/Modern-Bootcamp/Refresh/section3-5/number-methods.js
@@ -0,0 +1,19 @@
+// Exploring Number Methods
+
+let num = 43.48;
+
+console.log(num.toFixed(0));
+
+// -> 43;
+
+let num1 = 43.48;
+
+console.log(num1.toFixed(2));
+
+// -> 43.48;
+
+let num2 = 43.48;
+
+// console.log(num2.toFixed(-1));
+
+// -> RangeError: toFixed() digits argument must be between 0 and 100
