ebad5ea1d7c0b03ee9a42c46b506b3051304df71
Justin24953 <121590009+Justin24953@users.noreply.github.com>
2023-06-24 21:01:00 -0400

    Output Test For VS Code

---
+++ WeeklyCoding/interview-prep/Git/git-practice.js
@@ -0,0 +1,16 @@
+// write a function that returns the string, "hello, interview unit-1" 
+
+function sayHello() {
+  return "hello, interview unit-1"
+}
+
+// test
+console.log(sayHello())
+
+// run the program (look it up!)
+// run git add .
+// run git commit -m "interview exercise"
+// run git push origin 
+
+
+// What Is Git? 
