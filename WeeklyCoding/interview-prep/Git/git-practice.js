ccf429156e13ab6bd5d0122ddcce670ac808f253
Justin24953 <121590009+Justin24953@users.noreply.github.com>
2023-06-24 22:18:09 -0400

    Git Notes

--- WeeklyCoding/interview-prep/Git/git-practice.js
+++ WeeklyCoding/interview-prep/Git/git-practice.js
@@ -14,3 +14,36 @@ console.log(sayHello())
 
 
 // What Is Git? 
+
+/* [Git Is a Version (Control System)] */
+
+// What is a Version (Control System)? 
+
+// [Version Control Is A Way That Programmers Track Our Code Changes.]
+
+// ^  We do this to track down bugs or return back to a previous version of the code.
+
+/* Terms
+
+* Directory -> Folder
+*Terminal / Command Line -> Interface for Text Commands
+* CLI -> Command Line Interface
+*cd -> Change Directory 
+* Repository -> Project, or the folder/place where your project is kept.
+* Github -> A Website to  host your repositories */
+
+/* Git Commands 
+
+* clone -> Bring a repo that is hosted somewhere like Github into a folder on your local machine
+
+* add -> Track your files and changes in Git
+
+* commit -> Save your files in Git.
+
+* push -> Upload Git Commits to a remote repo like Github
+
+* pull -> Download changes from remote repo to your local machine, the opposite of push.
+
+
+
+
