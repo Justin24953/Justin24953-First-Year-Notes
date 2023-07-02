123075011c36ba018f51bbb4d7ce5ed4a3c86d23
Justin24953 <121590009+Justin24953@users.noreply.github.com>
2023-07-01 23:53:02 -0400

    Eloquent JavaScript

--- Eloquent-Javascript/values-types.js
+++ Eloquent-Javascript/values-types.js
@@ -2,4 +2,24 @@
 
 - Master Yuan-Ma, The Book of Programming */
 
-// Git Commit Test
+/* NaN stands for “not a number”
+
+^ You’ll get this result when you, for example, try to calculate 0 / 0 (zero divided by zero), Infinity - Infinity, or any number of other numeric operations that don’t yield a meaningful result. */
+
+let ifNan = 0 / -0
+
+console.log(ifNan)
+
+let forwardSlash =  'This is the first line \nThis is the second line'
+
+console.log(forwardSlash)
+
+function logString(string) {
+ return console.log(string)
+}
+
+logString('d')
+
+logString("First Test Case.\nSecond Line ")
+
+logString(`Backtic Test Case.\nSecond Line`)
