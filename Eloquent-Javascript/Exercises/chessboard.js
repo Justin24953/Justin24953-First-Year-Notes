1ed482a56c79d7f1f1eba4e81172e05515869ee3
Justin24953 <121590009+Justin24953@users.noreply.github.com>
2023-07-14 22:01:04 -0400

    ChessBoard Excercise

---
+++ Eloquent-Javascript/Exercises/chessboard.js
@@ -0,0 +1,28 @@
+/* Write a program that creates a string that represents an 8×8 grid, using newline characters to separate lines. At each position of the grid there is either a space or a "#" character. The characters should form a chessboard.
+
+Passing this string to console.log should show something like this:
+
+ # # # #
+# # # # 
+ # # # #
+# # # # 
+ # # # #
+# # # # 
+ # # # #
+# # # #
+
+When you have a program that generates this pattern, define a binding size = 8 and change the program so that it works for any size, outputting a grid of the given width and height. */
+
+// Create a String that creates a (8 x 8) grid (variable). 
+// Use newline chracters tp separate lines.
+// At Each Postion of the grid there is either a space or a '#' character.
+
+function createGrid (characters,symbol,space) {
+  let grid = ` `
+  for (let i = 0; i === characters;) {
+
+  }
+}
+
+console.log(createGrid(8,'#',' '))
+
